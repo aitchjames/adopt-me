8c548b0c276ed59314bc993376a53006e8015dde
Hayden <hayden@aitch.co>
2020-01-23 18:36:53 +1000

    added form for text input - no binding yet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Adopt Me"),
-        React.createElement(Pet, {
-            name: "Moon",
-            animal: "Dog",
-            breed: "Jack Russel"
-        }),
-        React.createElement(Pet, {
-            name: "Peeper",
-            animal: "Cat",
-            breed: "Cats have breeds?"
-        }),
-        React.createElement(Pet, {
-            name: "Salt",
-            animal: "Lizard",
-            breed: "Gecko"
-        })
-    ]);
+    return (
+        <div>
+            <h1>Adopt Me</h1>
+            <SearchParams />
+        </div>
+    );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
